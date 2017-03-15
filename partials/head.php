78bfba392126d3aa8f868a9ea05fda77ba1e8045
Justin Etzine <justetz@gmail.com>
2017-03-14 20:06:40 -0400

    Gave up on that, added compiled css to repo

--- partials/head.php
+++ partials/head.php
@@ -2,10 +2,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <title>RPI Student Government</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i,900" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link href="/styles/main.min.css" rel="stylesheet">
+    <link href="/styles/compiled.php" rel="stylesheet">
 </head>
