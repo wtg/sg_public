e76294f9725aa2a745e5fe72cd809abecd55fd46
Justin Etzine <justetz@gmail.com>
2017-04-20 22:07:00 -0400

    Added templates for both the project and updates pages

---
+++ projects/index.php
@@ -0,0 +1,243 @@
+
+<!DOCTYPE html>
+<html>
+<?php require_once '../partials/head.php' ?>
+<body>
+    <?php require_once '../partials/nav.php'; ?>
+    <main class="container">
+        <section class="row">
+            <div class="col-xs-12">
+                <h1>Projects</h1>
+            </div>
+        </section>
+        <section class="row">
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/senate">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Counseling Center Improvements</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/eboard">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Study Spaces Creation and Advocacy</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/jboard">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Increase of Student Use of EMPAC</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>On-Campus Pharmacy Creation</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-lg-offset-0 col-md-6 col-md-offset-3">
+                <a class="person-item" href="/people/gc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Preferred Names Project</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-fifth col-md-6">
+                <a class="person-item" href="/people/uc">
+                    <div class="person-image oblong">
+                    </div>
+                    <div class="person-content">
+                        <h4>Insert Project Name Here</h4>
+                    </div>
+                </a>
+            </div>
+        </section>
+    </main>
+    <?php require_once '../partials/footer.php' ?>
+</body>
+</html>
---
+++ updates/index.php
@@ -0,0 +1,105 @@
+
+<!DOCTYPE html>
+<html>
+<?php require_once '../partials/head.php' ?>
+<body>
+    <?php require_once '../partials/nav.php'; ?>
+    <style type="text/css">
+        /* I know this is bad and should not be here. It's short term, don't judge! */
+
+        a {
+            color: #c40000;
+        }
+    </style>
+    <main class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>Updates</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <article>
+                    <h2>Senate Meeting – Monday, April 17</h2>
+                    <p class="small text-muted">
+                         4 days ago
+                    </p>
+                    <p class="lead">On Monday, the Senate will be meeting in the Shelnutt Gallery in the Union (RU 3606) at 8 pm. This meeting will include discussions and votes on:</p>
+                    <ul>
+                        <li>A <a href="https://rpi.app.box.com/s/8dhvnrimse57116d5u2q0jgtouile47f">special elections handbook</a> for the Greek Senator race, and</li>
+                        <li>Confirmation of the Grand Marshal’s Senate officer appointments.</li>
+                    </ul>
+
+                    <p class="lead">The appointments... <a href="#">Read more</a></p>
+                </article>
+                <hr />
+                <article>
+                    <h2>Bookstore Discussion 5-15-2015</h2>
+                    <p class="small text-muted">
+                         2 years ago
+                    </p>
+                    <p class="lead">
+                        On Friday, May 15th, the Executive Board convened for a meeting with the orders of business being the confirmation of appointments of officers and committee chairs, and a report on the bookstore. All of the appointments were approved swiftly, and the discussion turned to the bookstore.
+                    </p>
+                    <p class="lead">125th President of the Union Erin Amarello... <a href="#">Read more</a></p>
+                </article>
+            </div>
+            <div class="col-md-3 col-md-offset-1">
+                <h3>Filter by body</h3>
+                <ul>
+                    <li>
+                        <a href=""><strong>All</strong></a>
+                    </li>
+                    <li>
+                        <a href="">Senate</a>
+                    </li>
+                    <li>
+                        <a href="">Executive Board</a>
+                    </li>
+                    <li>
+                        <a href="">Judicial Board</a>
+                    </li>
+                    <li>
+                        <a href="">Undergraduate Council</a>
+                    </li>
+                    <li>
+                        <a href="">Graduate Council</a>
+                    </li>
+                </ul>
+                <hr />
+                <h3>Filter by committee or council</h3>
+                <ul>
+                    <li>
+                        <a href=""><strong>All</strong></a>
+                    </li>
+                    <li>
+                        <a href="">Student Life Committee</a>
+                    </li>
+                    <li>
+                        <a href="">Academic Affairs Committee</a>
+                    </li>
+                    <li>
+                        <a href="">Facilities and Services Committee</a>
+                    </li>
+                    <li>
+                        <a href="">Union Policies Committee</a>
+                    </li>
+                    <li>
+                        <a href="">Class of 2018 Council</a>
+                    </li>
+                    <li>
+                        <a href="">Class of 2019 Council</a>
+                    </li>
+                    <li>
+                        <a href="">Class of 2020 Council</a>
+                    </li>
+                    <li>
+                        <a href="">Class of 2021 Council</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </main>
+    <?php require_once '../partials/footer.php' ?>
+</body>
+</html>
