0cd6d73e1a97ac97d34f555dedf0d1c2643b30b0
Justin Etzine <justetz@gmail.com>
2017-12-09 22:52:13 -0500

    Amended debug based on previous test

--- actions/index.php
+++ actions/index.php
@@ -113,7 +113,7 @@ $activeSessions = Sessions::read([
                         echo "<h3>No actions were found!</h3>";
                     } else {
                         $i = 0;
-                        echo $actions;
+                        print_r($actions);
                         foreach($actions as $entry) {
                             if($i > 0) {
                                 echo "<hr />";
